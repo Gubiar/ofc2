5d62ed7df56cb6382bae0531a86e5829820d8404
Gubiar <gubiar.dev@gmail.com>
2023-04-26 13:38:28 -0300

    moved Login.dart to PageLogin.dart

---
+++ lib/screens/PageLogin.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:ofc2/screens/PageHome.dart';
+
+class PageLogin extends StatefulWidget {
+  const PageLogin({Key? key}) : super(key: key);
+
+  @override
+  State<PageLogin> createState() => _PageLoginState();
+}
+
+class _PageLoginState extends State<PageLogin> {
+
+  buttonPressed(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const PageHome()),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Usuário'),
+      ),
+      body:
+      Container(
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
+        child:
+        Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Faça Login",
+                style: TextStyle(fontSize:47.0,
+                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w900,
+                    fontFamily: "Roboto"),
+              ),
+
+              const Text(
+                "Email",
+                style: TextStyle(fontSize:12.0,
+                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+
+              const TextField(
+                style: TextStyle(fontSize:12.0,
+                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+
+              const Text(
+                "senha",
+                style: TextStyle(fontSize:12.0,
+                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+
+              const TextField(
+                style: TextStyle(fontSize:12.0,
+                    color: Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+
+              ElevatedButton(key:null, onPressed:buttonPressed,
+                  style: ElevatedButton.styleFrom( backgroundColor: const Color(0xFFe0e0e0),),
+                  child:
+                  const Text(
+                    "login",
+                    style: TextStyle(fontSize:23.0,
+                        color: Color(0xFF000000),
+                        fontWeight: FontWeight.w900,
+                        fontFamily: "Roboto"),
+                  )
+              ),
+
+              const Text(
+                "não tem conta? clique aqui",
+                style: TextStyle(fontSize:13.0,
+                    color: Color(0xFFb3b3b3),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Roboto"),
+              )
+            ]
+
+        ),
+      ),
+
+    );
+  }
+}
